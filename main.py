917cb4f89dcda8a4485b2d0ea7a1facdbdf5b082
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2022-05-24 11:43:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from tkinter import *
+
+path = "C:/Users/21263/Downloads"
+files =os.listdir(path)
+
+def Orgenize():
+    for file in files:
+        filename,extension=os.path.splitext(file)
+        extension=extension[1:]
+
+        if file.endswith(".mp4") or file.endswith(".mkv"):
+            shutil.move(path+"/"+file,path+"/video")
+        else:
+            if os.path.exists(path+'/'+extension):
+              shutil.move(path+"/"+file,path+"/"+extension+"/"+file)
+            else:
+              os.makedirs(path+"/"+extension)
+              shutil.move(path+"/"+file,path+"/"+extension+"/"+file)
+
+if __name__=="__main__":
+    root = Tk()
+
+    root.title("File Orgenizer")
+
+    orgeniz=Button(root,padx=40,pady=16,bd=8,fg="black",text="Orgenize",command=lambda:Orgenize()).grid(row=1,column=0)
+
+    root.mainloop()
